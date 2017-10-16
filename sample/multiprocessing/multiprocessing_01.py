b5a430c9d6c73d5f277ec0eb42d97070bc144498
Win10 <rannrunn@hanmail.net>
2017-10-17 04:17:32 +0900

    buble sort algorithm commit

--- sample/multiprocessing/multiprocessing_01.py
+++ sample/multiprocessing/multiprocessing_01.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     numbers = [5, 10, 15, 20, 25]
     procs = []
 
+    # 프로세스 중 하나에 에러가 생길 경우 그 프로세스만 중단된다. (에러처리하지 않을 경우)
     for index, number in enumerate(numbers):
         proc = Process(target=doubler, args=(number,))
         procs.append(proc)
