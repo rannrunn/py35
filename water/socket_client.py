d9baf76ec243dc37c09764904549a7d105ddb74e
rannrunn <rannrunn@hanmail.net>
2017-10-30 11:45:27 +0900

    kepco_iot push

--- water/socket_client.py
+++ water/socket_client.py
@@ -7,11 +7,50 @@ def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('localhost', 12580))
 
-    data = {}
-    # abnormal_judgment , learning_start, learning_add, predict_discharge
-    data['command'] = 'learning_start[]'
 
-    json_data = json.dumps(data)
+    # calculate_statistics_abnormal
+    # calculate_abnormal
+    # calculate_statistics
+    # calculate_regression
+    command = 'calculate_statistics'
+    # average, variance, standard_deviation,
+    command_detail = 'correlation'
+
+    dict = {}
+    dict['command'] = command
+    dict['command_to'] = 'server'
+    time_start = '2017-07-01 00:00:00'
+    time_end = '2017-08-01 00:00:00'
+
+
+    if command == 'calculate_statistics_abnormal':
+        dict['section'] = 'one'
+        dict['time_start'] = time_start
+        dict['time_end'] = time_end
+    elif command == 'calculate_abnormal':
+        dict['section_one_standard_discharge'] = '1539'
+        dict['section_two_standard_discharge'] = '3981'
+    elif command == 'calculate_statistics':
+        if command_detail == 'correlation':
+            dict['command_detail'] = 'correlation'
+            dict['location_one'] = {'location':'seongsan', 'type':'discharge'}
+            dict['location_two'] = {'location':'gimpo', 'type':'pressure'}
+            dict['time_start'] = time_start
+            dict['time_end'] = time_end
+        else:
+            dict['command_detail'] = command_detail
+            dict['location'] = 'd.seongsan - d.gimpo'
+            dict['time_start'] = time_start
+            dict['time_end'] = time_end
+    elif command == 'calculate_regression':
+        dict['location_source'] = '[d.yangje - d.gwangam][d.yangje - d.gimpo]'
+        dict['location_target'] = 'd.paldang + d.36t'
+        dict['time_start'] = time_start
+        dict['time_end'] = time_end
+
+    # abnormal_judgment , learning_start, learning_add, predict_discharge
+
+    json_data = json.dumps(dict)
 
     print('client : JSON_print: ', json_data)
 
