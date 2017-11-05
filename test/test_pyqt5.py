0f984dcb4c061d184dca1bf64db542cf5516223a
Win10 <rannrunn@hanmail.net>
2017-11-05 17:06:00 +0900

    buble sort algorithm commit

---
+++ test/test_pyqt5.py
@@ -0,0 +1,10 @@
+import sys
+
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QInputDialog, QApplication)
+
+app = QApplication([])
+dialog = QInputDialog()
+dialog.show()
+app.exec_()
+
+
