c721eeb542a3d060f14d140fb7923bc4c8bad0aa
rannrunn <rannrunn@hanmail.net>
2018-05-21 17:53:40 +0900

    modify IoT

--- project/power/iot/iot_analysis/iot_plot_graph.py
+++ project/power/iot/iot_analysis/iot_plot_graph.py
@@ -6,11 +6,10 @@ import os
 from multiprocessing import Pool
 from matplotlib import font_manager, rc
 import numpy as np
-import seaborn as sns
 import time
-from iot_analysis_util import preprocess as pre
+from iot_util import iot_preprocess as pre
 
-font_name = font_manager.FontProperties(fname="c:\\Windows\\Fonts\\NGULIM.TTF").get_name()
+font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/NGULIM.TTF").get_name()
 rc('font', family=font_name)
 
 def get_list_from_location(df_sensor_info, location):
@@ -62,49 +61,14 @@ def get_list_from_manufacturer(df_sensor_info, manufacturer_number):
     return list
 
 
-def make_plot(dict):
-    file_path = dict['file_path']
-    pole_id = dict['pole_id']
-    plot_type = dict['plot_type']
-    resample_how = dict['resample_how']
-    item_location = dict['item_location']
-    item_manufacturer = dict['item_manufacturer']
-    item_mounting_position = dict['item_mounting_position']
-
-    df = pd.read_csv(file_path, encoding='euckr')
+def read_csv(file_name):
+    df = pd.read_csv(file_name, encoding='euckr')
     df = pre.remove_sensor_malfunction_data(df)
     df = pre.remove_out_of_range_data(df)
     df.set_index('TIME_ID', inplace=True)
     df.index = pd.to_datetime(df.index)
-    df.index.rename(file_path[file_path.rfind('_') + 1:file_path.rfind('.')], inplace=True)
-    df = df.drop(['PERIOD', 'SHOCK'], axis=1)
-    df = df.dropna(axis=1, how='all')
-    fig = plt.figure(figsize=(24, 7))
-    fig.suptitle(item_location + '_' + item_manufacturer + '_' + item_mounting_position + '_' + pole_id + '_' + df.index.name)
-    cmap = sns.diverging_palette(220, 10, as_cmap=True)
-
-    plt.subplot(131)
-    plt.title('\n\n1 Min')
-    corr_1_min = df.resample('1Min').mean().corr()
-    mask = np.zeros_like(corr_1_min, dtype=np.bool)
-    mask[np.triu_indices_from(mask)] = True
-    sns.heatmap(corr_1_min, mask=mask, cmap=cmap, vmax=1, vmin=-1, center=0, annot=True, fmt='0.2f')
-
-    plt.subplot(132)
-    plt.title('\n\n1 Day')
-    corr_1_day = df.resample('1D').mean().corr()
-    sns.heatmap(corr_1_day, mask=mask, cmap=cmap, vmax=1, vmin=-1, center=0, annot=True, fmt='0.2f')
-
-    plt.subplot(133)
-    plt.title('\n\n1 Month')
-    corr_1_month = df.resample('1M').mean().corr()
-    sns.heatmap(corr_1_month, mask=mask, cmap=cmap, vmax=1, vmin=-1, center=0, annot=True, fmt='0.2f')
-
-    plt.savefig('C:\\_data\\output\\' + plot_type + '\\' + resample_how + '\\' + item_location + '\\' + item_manufacturer + '\\' + item_mounting_position + '\\' + pole_id + '_' + df.index.name + '.png', bbox_inches='tight')
-    # plt.show()
-    plt.close()
-    print(item_location + '_' + item_manufacturer + '_' + item_mounting_position + '_' + pole_id + '_' + df.index.name)
-
+    df.index.rename(file_name[file_name.rfind('_') + 1:file_name.rfind('.')], inplace=True)
+    return df
 
 # resample 을 하기 전에 오류 값을 제거해야 함
 # TEMP,HUMI,PITCH,ROLL,AMBIENT,UV,PRESS,BATTERY,PERIOD,CURRENT,SHOCK,GEOMAG_X,GEOMAG_Y,GEOMAG_Z,VAR_X,VAR_Y,VAR_Z,USN,NTC,UVC
@@ -112,18 +76,18 @@ if __name__ == '__main__':
 
     start = time.time()
 
-    plot_type = 'correlation'
+    plot_type = 'graph'
+    resample_how = '30min'
 
-    dir = 'C:\\_data\\output_db_file_pi'
+    dir = 'C:/_data/output_db_file_pi'
 
     list_location = ['고창', '광주', '대구', '대전', '안산']
     list_mounting_position = ['변압기 본체', '부하 개폐기', '완금', '전주', '통신용 함체']
     list_variable = ['TEMP', 'HUMI', 'PITCH', 'ROLL', 'AMBIENT', 'UV', 'PRESS', 'BATTERY', 'PERIOD', 'CURRENT', 'SHOCK', 'GEOMAG_X', 'GEOMAG_Y', 'GEOMAG_Z', 'VAR_X', 'VAR_Y', 'VAR_Z', 'USN', 'NTC', 'UVC']
     list_manufacturer_number = ['1', '4', '6']
-    resample_how = '1Min_1Day_1Month'
 
     # pole information read
-    df_sensor_info = pd.read_csv('C:\\_data\\iot_sensor_info.csv', encoding='euckr')
+    df_sensor_info = pd.read_csv('C:/_data/iot_sensor_info.csv', encoding='euckr')
     df_sensor_info = df_sensor_info[df_sensor_info['MOUNTING_POSITION'].notnull()]
     df_sensor_info['FILE_NAME'] = df_sensor_info['SENSOR_OID'].str.slice(10, 11) + '_' + df_sensor_info['POLE_CPTZ_NO'] + '_' + df_sensor_info['SENSOR_OID'] + '.csv'
 
@@ -138,7 +102,7 @@ if __name__ == '__main__':
                 list_result = [item for item in list_result if item[item.rfind('_') + 1:item.rfind('.')] in list_from_manufacturer]
 
 
-                list_file_path = []
+                list_file_name = []
                 cnt = 0
                 for path, dirs, files in os.walk(dir):
                     # print(path)
@@ -146,35 +110,52 @@ if __name__ == '__main__':
                         if file in list_result:
                             cnt += 1
                             # print('FILE_NAME:', file)
-                            list_file_path.append(os.path.join(path, file))
-
+                            list_file_name.append(os.path.join(path, file))
 
                 # 아웃풋 디렉토리 체크
-                dir_output = 'C:\\_data\\output\\{}\\{}\\{}\\{}\\{}'.format(plot_type, resample_how, item_location, item_manufacturer, item_mounting_position)
+                dir_output = 'C:/_data/output/{}/{}/{}/{}/{}'.format(plot_type, resample_how, item_location, item_manufacturer, item_mounting_position)
                 if not os.path.isdir(dir_output):
                     os.makedirs(dir_output)
 
-
-                cmt_image = 100
-                for idx in range(0, len(list_file_path), cmt_image):
-                    list_dict = []
-                    for idx_two in range(idx, min(len(list_file_path), idx + cmt_image) , 1):
-                        file_name = os.path.basename(list_file_path[idx_two])
-                        dict = {}
-                        dict['file_path'] = list_file_path[idx_two]
-                        if list_file_path[idx_two].find('_') + 1 == list_file_path[idx_two].rfind('_'):
-                            dict['pole_id'] = ''
-                        else:
-                            dict['pole_id'] = file_name[file_name.find('_') + 1:file_name.rfind('_')]
-                        dict['plot_type'] = plot_type
-                        dict['resample_how'] = resample_how
-                        dict['item_location'] = item_location
-                        dict['item_manufacturer'] = item_manufacturer
-                        dict['item_mounting_position'] = item_mounting_position
-                        list_dict.append(dict)
+                cmt_image = 10
+                for idx in range(0, len(list_file_name), cmt_image):
+                    list_file_name_part = list_file_name[idx:idx + (cmt_image - 1)]
 
                     with Pool(processes=16) as pool:
-                        pool.map(make_plot, list_dict)
+                        list_df = pool.map(read_csv, list_file_name_part)
                         pool.close()
+                        pool.join()
+
+                    for item_variable in list_variable:
+                        print(item_location + '_' + item_manufacturer + '_' + item_mounting_position + '_' + item_variable + ',', '리스트 길이:', len(list_result))
+
+                        df = pd.DataFrame()
+                        df.index.name = 'TIME_ID'
+
+                        bool_plot = False
+                        for item in list_df:
+                            df_item = pd.DataFrame(item[item_variable])
+                            df_item = df_item[df_item[item_variable].notnull()]
+                            if len(df_item) > 0:
+                                if bool_plot == False:
+                                    plt.figure(figsize=(10, 4))
+                                df_item = df_item.resample(resample_how).mean()
+                                plt.plot(df_item.index.values, df_item.iloc[:, 0].values, label=df_item.index.name, linewidth=0.7)
+                                bool_plot = True
+
+                        if bool_plot == True:
+                            legned = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+                            plt.title(item_location + '_' + item_manufacturer + '_' + item_mounting_position + '_' + item_variable)
+                            plt.xticks(rotation=30)
+                            plt.savefig('C:/_data/output/' + plot_type + '/' + resample_how + '/' + item_location + '/' + item_manufacturer + '/' + item_mounting_position + '/' + item_variable + '_' + str(idx + cmt_image) + '.png', bbox_extra_artists=(legned,), bbox_inches='tight')
+                            plt.close()
+                        else:
+                            print(item_location + '_' + item_manufacturer + '_' + item_mounting_position + '_' + item_variable + ',', 'CNT:', idx + cmt_image, ', 데이터가 없습니다.')
 
     print('Total Time:{}'.format(str(round(time.time() - start))))
+
+# x축 고정
+# y축 고정
+# y축 제한
+# 측정 예상치를 벗어나는 데이터 제거
+
