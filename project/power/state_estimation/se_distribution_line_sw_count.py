e88bad16db92f1d83e4fb39e98cf3b4d1aea409c
rannrunn <rannrunn@hanmail.net>
2018-05-29 17:01:00 +0900

    Modify : Distribution Line

--- project/power/state_estimation/se_distribution_line_sw_count.py
+++ project/power/state_estimation/se_distribution_line_sw_count.py
@@ -6,11 +6,12 @@ pd.set_option('max_columns', 30)
 import os
 
 
-def function(dl_id, sw_id_f, multi_flag, list_sw):
+# 다중화 개폐기가 아닌 경우에도 SW_FRTU 테이블에 정보가 있을 수 있음을 고려
+def function(dl_id, sw_id_f, multi_flag, list_sw, list_text):
     # 같은 sw가 SEC 테이블에서 들어온 경우('.0'이 있음)와 SW_FRTU 테이블에서 들어온 경우('.0'이 없음)를 구분하여 조건을 따짐
     # (의도한 것은 아니지만 구현이 필요한 알고리즘이 자동으로 해결되었음)
     if str(sw_id_f) in list_sw and str(sw_id_f) != 'nan':
-        # print(str(sw_id_f) + '가 반복됩니다.')
+        list_text.append(str(sw_id_f) + '가 반복됩니다.')
         return list_sw
     # 다중화 개폐기에 속한 개폐기 중 인풋 개폐기를 제외한 나머지 개폐기들은 리스트에 추가시키지 않음
     if multi_flag == False:
@@ -19,30 +20,42 @@ def function(dl_id, sw_id_f, multi_flag, list_sw):
         return list_sw
     df_local_sec = df_sec.loc[df_sec['sw_id_f'] == sw_id_f]
     df_local_sw_frtu = df_sw_frtu[(df_sw_frtu['dl_id'] == dl_id) & (df_sw_frtu['sw_id'] == sw_id_f)]
-    if len(df_local_sec) != 0:
-        sw_id_b = df_local_sec['sw_id_b'].values[0]
-        # print('sw_id_f:', sw_id_f, 'sw_id_b:', sw_id_b)
-        function(dl_id, sw_id_b, False, list_sw)
-    # 다중화 개폐기의 인풋이 바로 아웃풋인 경우가 있는 지 확인해 봐야 함
-    elif len(df_local_sw_frtu) > 0 and multi_flag == False:
+    sr_sw_id = pd.DataFrame()
+    if len(df_local_sw_frtu) > 0:
         sw_loc = df_local_sw_frtu['sw_loc'].values[0]
         sw_loc = sw_loc[:sw_loc.find('(')] if sw_loc.find('(') > -1 else sw_loc
         sr_sw_id = df_sw_frtu[df_sw_frtu['sw_loc'].apply(lambda x: x[:x.find('(')] if x.find('(') > -1 else x) == sw_loc]['sw_id']
-        # print('개폐기 인풋:', sw_id_f)
-        # print('개폐기 로케이션:', sw_loc)
-        # print('개폐기 세부 개폐기:')
-        # for val in sr_sw_id:
-        #     print(val)
-        # print('개폐기 끝')
-        for val in sr_sw_id:
-            function(dl_id, val, True, list_sw)
-        # print('있음')
+
+    # 다중화 개폐기의 인풋이 바로 아웃풋인 경우가 있는 지 확인해 봐야 함 : 없는 듯
+    # SW_FRTU 테이블에 DL_ID가 같고, 같은 그룹에 속한 SW 가 1개를 초과할 경우에만 다중화 개폐기 SW 탐색
+    # 다중화 개폐기 탐색을 통해 함수가 실행된 경우에 다시 다중화 개폐기 탐색을 할 경우 LOOP 가 발생하므로 이 경우는 패스
+    if len(sr_sw_id) > 1 and multi_flag == False:
+        list_text.append('개폐기 인풋: ' + str(sw_id_f))
+        list_text.append('개폐기 로케이션: ' + str(sw_loc))
+        list_text.append('개폐기 세부 개폐기:')
+        for sw_id_b in sr_sw_id:
+            list_text.append(str(sw_id_b))
+        list_text.append('개폐기 끝')
+        for sw_id_b in sr_sw_id:
+            function(dl_id, sw_id_b, True, list_sw, list_text)
+    elif len(df_local_sec) != 0 & ((len(df_sw_frtu[(df_sw_frtu['sw_id'] == sw_id_f)]) == 0) | (len(df_local_sw_frtu) != 0)):
+        sr_sw_id_b = df_local_sec['sw_id_b']
+        for sw_id_b in sr_sw_id_b:
+            list_text.append('sw_id_f: ' + str(sw_id_f) + ', sw_id_b: ' + str(sw_id_b))
+            function(dl_id, sw_id_b, False, list_sw, list_text)
     return list_sw
 
 
 if __name__ == '__main__':
 
-    x1 = pd.ExcelFile('C:\\_data\\das_data.xls')
+    dir = 'C:\\_data'
+    dir_distribution_line = dir + '\\distribution_line'
+    dir_distribution_line_topology = dir_distribution_line + '\\topology'
+
+    x1 = pd.ExcelFile(os.path.join(dir, 'das_data_20180529.xls'))
+
+    if not os.path.isdir(dir_distribution_line_topology):
+        os.makedirs(dir_distribution_line_topology)
 
     # print(x1.sheet_names)
 
@@ -61,23 +74,27 @@ if __name__ == '__main__':
     # DL 9 는 SW_FRTU 테이블에 개폐기가 여러개 있으나 연결이 끊기니 구현된 단선도 프로그램을 확인해 봐야함
     # 18 : 다중화 개폐기 4개
 
-
     df_dl_line_count = pd.DataFrame(columns=['DL_ID', 'DL_NAME', 'CB_ID', 'COUNT'])
+
     for idx in range(len(df_dl)):
 
         dl_id = df_dl.loc[idx, 'dl_id']
         dl_name = df_dl.loc[idx, 'dl_name']
+
+        # if dl_id != 6:
+        #     continue
 
         cb_id = None
         if len(df_dl.loc[df_dl['dl_id'] == dl_id, 'cb_id']) > 0:
             cb_id = df_dl.loc[df_dl['dl_id'] == dl_id, 'cb_id'].values[0]
         else:
             print('dl_id가 없습니다.')
-            continue
+
 
         list_sw = []
+        list_text = []
         if dl_id is not None and cb_id is not None:
-            function(dl_id, cb_id, False, list_sw)
+            function(dl_id, cb_id, False, list_sw, list_text)
 
         for idx_2, val in  enumerate(list_sw):
             list_sw[idx_2] = list_sw[idx_2].replace('.0', '')
@@ -86,10 +103,12 @@ if __name__ == '__main__':
         df_temp = pd.DataFrame([[dl_id, dl_name, cb_id, len(list_sw)]], columns=['DL_ID', 'DL_NAME', 'CB_ID', 'COUNT'])
         df_dl_line_count = df_dl_line_count.append(df_temp, ignore_index=True)
 
-        print('idx:', idx)
+        print('idx: ' + str(idx) + ', dl_id: ' + str(dl_id) + ', dl_name: ' + str(dl_name))
 
-    dir = 'C:\\_data'
+        with open(os.path.join(dir_distribution_line_topology, str(dl_id) + '_' + dl_name + '.txt'), 'w') as file:
+            file.writelines(['%s\n' % item for item in list_text])
+
     if not os.path.isdir(dir):
         os.makedirs(dir)
-    df_dl_line_count.to_csv(os.path.join(dir, 'dl_sw_count.csv'), index=False)
+    df_dl_line_count.to_csv(os.path.join(dir_distribution_line, 'dl_sw_count.csv'), index=False)
 
