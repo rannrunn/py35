3c63b718d491696eb017247fb724daf06fc62731
rannrunn <rannrunn@hanmail.net>
2018-08-07 17:54:24 +0900

    Modify : PQMS Change Detection

--- project/power/state_estimation/pqms_change_detection/pqms_change_detection_all.py
+++ project/power/state_estimation/pqms_change_detection/pqms_change_detection_all.py
@@ -58,7 +58,7 @@ def regularization(df, col, col_reg):
 
 def plot_pqms_decomposition(period):
     dir_source = 'C:\\_data\\부하데이터'
-    dir_output = 'C:\\_data\\pqms_change_detection_all_minmax\\' + str(period) + '\\'
+    dir_output = 'C:\\_data\\pqms_change_detection_all_many_2\\' + str(period) + '\\'
 
     if not os.path.isdir(dir_output):
         os.makedirs(dir_output)
@@ -89,28 +89,49 @@ def plot_pqms_decomposition(period):
                 df_data_decom['resid'] = data_decom.resid
 
                 df_data_decom = regularization(df_data_decom, 'resid', 'reg_resid')
-                th_reg_resid = np.abs(df_data_decom['reg_resid'].mean()) + (df_data_decom['reg_resid'].std() * 1.5)
-                df_data_decom['diff_observed'] = df_data_decom['observed'].diff().abs()
-                df_data_decom['diff_reg_resid'] = df_data_decom['reg_resid'].diff().abs()
+                th_reg_resid = df_data_decom['reg_resid'].mean() + (df_data_decom['reg_resid'].std() * 1.5)
+                df_data_decom['diff_observed'] = df_data_decom['observed'].diff()
+                df_data_decom['diff_reg_resid'] = df_data_decom['reg_resid'].diff()
                 df_data_decom['resid_change_detection'] = 0
                 df_data_decom.loc[(df_data_decom['reg_resid'].abs() > th_reg_resid), 'resid_change_detection'] = 1
 
+
+                # 주별 통계량
                 df_data_decom['datetime'] = df_data_decom.index.values
                 df_data_decom['weekday'] = df_data_decom['datetime'].dt.weekday
                 df_data_decom['day_of_year'] = df_data_decom['datetime'].dt.dayofyear
                 df_data_decom['day_of_years'] = df_data_decom['datetime'].apply(lambda x: df_data_decom.loc[x, 'day_of_year'] + sum([datetime.date(item, 12, 31).timetuple().tm_yday for item in list(set(df_data_decom['datetime'].dt.year.values)) if item < df_data_decom.loc[x, 'datetime'].year]))
 
 
-                df_data_decom['date_group'] = df_data_decom['datetime'].apply(lambda x: df_data_decom.loc[x, 'day_of_years'] - (df_data_decom.loc[x, 'weekday'] + 1))
-                df_data_decom = df_data_decom[(df_data_decom['date_group'] != df_data_decom['date_group'].min()) & (df_data_decom['date_group'] != df_data_decom['date_group'].max())]
+                df_data_decom['date_group_week'] = df_data_decom['datetime'].apply(lambda x: df_data_decom.loc[x, 'day_of_years'] - (df_data_decom.loc[x, 'weekday'] + 1))
+                df_data_decom = df_data_decom[(df_data_decom['date_group_week'] != df_data_decom['date_group_week'].min()) & (df_data_decom['date_group_week'] != df_data_decom['date_group_week'].max())]
 
-                df_data_decom['data_week_odd'] = df_data_decom.loc[df_data_decom['date_group'] % 2 == 0, 'observed']
-                df_data_decom['data_week_even'] = df_data_decom.loc[df_data_decom['date_group'] % 2 == 1, 'observed']
+                df_data_decom['data_week_odd'] = df_data_decom.loc[df_data_decom['date_group_week'] % 2 == 0, 'observed']
+                df_data_decom['data_week_even'] = df_data_decom.loc[df_data_decom['date_group_week'] % 2 == 1, 'observed']
 
 
-                df_data_decom_des = pd.DataFrame(df_data_decom.groupby('date_group')['observed'].describe())
-                df_data_decom_des['date_group'] = df_data_decom_des.index
-                df_data_decom = pd.merge(df_data_decom, df_data_decom_des, on='date_group', how='left')
+                df_data_decom_des_week = pd.DataFrame(df_data_decom.groupby('date_group_week')['observed'].describe())
+                df_data_decom_des_week['date_group_week'] = df_data_decom_des_week.index
+                df_data_decom_des_week.rename(columns={'count': 'week_count', 'mean': 'week_mean', 'std': 'week_std', 'min': 'week_min', '25%': 'week_25%', '50%': 'week_50%', '75%': 'week_75%', 'max': 'week_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
+                df_data_decom = pd.merge(df_data_decom, df_data_decom_des_week, on='date_group_week', how='left')
+
+
+                df_data_decom['diff_week_max'] = df_data_decom['week_max'].diff()
+                df_data_decom['diff_week_mean'] = df_data_decom['week_mean'].diff()
+                df_data_decom['diff_week_min'] = df_data_decom['week_min'].diff()
+
+
+                # 일별 통계량
+                df_data_decom['yyyymmdd'] = df_data_decom['datetime'].dt.strftime('%Y%m%d')
+                df_data_decom_des_day = pd.DataFrame(df_data_decom.groupby('yyyymmdd')['observed'].describe())
+                df_data_decom_des_day['yyyymmdd'] = df_data_decom_des_day.index
+                df_data_decom_des_day.rename(columns={'count': 'day_count', 'mean': 'day_mean', 'std': 'day_std', 'min': 'day_min', '25%': 'day_25%', '50%': 'day_50%', '75%': 'day_75%', 'max': 'day_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
+                df_data_decom = pd.merge(df_data_decom, df_data_decom_des_day, on='yyyymmdd', how='left')
+
+                df_data_decom['diff_day_max'] = df_data_decom['day_max'].diff()
+                df_data_decom['diff_day_mean'] = df_data_decom['day_mean'].diff()
+                df_data_decom['diff_day_min'] = df_data_decom['day_min'].diff()
+
 
                 df_data_decom.set_index(df_data_decom['datetime'], inplace=True)
 
@@ -126,17 +147,6 @@ def plot_pqms_decomposition(period):
                 # plt.show()
                 # plt.close()
 
-
-                # # STL Decomposition Data Plot
-                # plt.plot(df_data_decom['observed'], label='observed')
-                # plt.plot(df_data_decom['trend'], label='trend')
-                # plt.plot(df_data_decom['detrend'], label='detrend')
-                # plt.plot(df_data_decom['seasonal'], label='seasonal')
-                # plt.plot(df_data_decom['resid'], label='resid')
-                # plt.legend()
-                # plt.show()
-                # plt.close()
-                #
                 # # 정규화 데이터 플롯
                 # plt.plot(df_data_decom['reg_resid'], label='reg_resid')
                 # plt.legend()
@@ -151,8 +161,8 @@ def plot_pqms_decomposition(period):
 
 
                 df_4H_mean = regularization(df_4H_mean, 'load', 'reg_load')
-                df_4H_mean['diff_load'] = df_4H_mean['load'].diff().abs()
-                df_4H_mean['diff_reg_load'] = df_4H_mean['reg_load'].diff().abs()
+                df_4H_mean['diff_load'] = df_4H_mean['load'].diff()
+                df_4H_mean['diff_reg_load'] = df_4H_mean['reg_load'].diff()
                 df_4H_mean['mean_change_detection'] = 0
                 # 4시간 부하 데이터가 이전 시점보다 특정 비율을 초과하여 변동한 시점 탐지
                 df_4H_mean.loc[(df_4H_mean['diff_reg_load'].abs() > 0.3) & (df_4H_mean['diff_load'].abs() > 2000), 'mean_change_detection'] = 1
@@ -165,8 +175,8 @@ def plot_pqms_decomposition(period):
                 # 일별 부하 데이터의 최대값이 임계치를 초과하여 변동한 시점 탐지
                 # 최소값은 거의 일정하므로 최대값만으로 탐지하면 될 것 같음
                 df_day_max = regularization(df_day_max, 'load', 'reg_load')
-                df_day_max['diff_load'] = df_day_max['load'].diff().abs()
-                df_day_max['diff_reg_load'] = df_day_max['reg_load'].diff().abs()
+                df_day_max['diff_load'] = df_day_max['load'].diff()
+                df_day_max['diff_reg_load'] = df_day_max['reg_load'].diff()
                 df_day_max['max_change_detection'] = 0
                 df_day_max.loc[(df_day_max['diff_reg_load'].abs() > 0.2) & (df_day_max['diff_load'].abs() > 2000), 'max_change_detection'] = 1
 
@@ -175,114 +185,96 @@ def plot_pqms_decomposition(period):
                 df_4H_mean.reindex([idx for idx in range(len(df_4H_mean))])
 
 
-                fig = plt.figure(figsize=(22,10))
-                gs = gridspec.GridSpec(4, 3)
-                ax1 = fig.add_subplot(gs[0, 0])
-                ax2 = fig.add_subplot(gs[0:2, 1])
-                ax3 = fig.add_subplot(4,3,3)
-                ax4 = fig.add_subplot(4,3,4)
+                fig = plt.figure(figsize=(35,14))
+                gs = gridspec.GridSpec(4, 5)
+                ax0_0 = plt.subplot(gs[0, 0])
+                ax1_0 = plt.subplot(gs[1, 0], sharex=ax0_0)
+                ax2_0 = plt.subplot(gs[2, 0], sharex=ax0_0)
+                ax3_0 = plt.subplot(gs[3, 0], sharex=ax0_0)
 
 
-                ax6 = fig.add_subplot(4,3,6)
-                ax7 = fig.add_subplot(4,3,7)
-                ax8 = fig.add_subplot(gs[2:4, 1])
-                ax9 = fig.add_subplot(4,3,9)
-                ax10 = fig.add_subplot(4,3,10)
-                ax12 = fig.add_subplot(4,3,12)
+                ax0_1 = plt.subplot(gs[0:2, 1])
+                ax2_1 = plt.subplot(gs[2:4, 1], sharex=ax0_0)
 
 
-                ax1.plot(df_4H_mean['load'], label='observed')
-                ax1.plot(data_decom.trend, label='trend')
-                # ax1.set_xticklabels(df_4H_mean['date'], rotation=30)
-                ax1.set_xticks(df_data_decom.index[::168])
-                ax1.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax1.legend()
-
-                ax4.plot(df_4H_mean['load'] - data_decom.trend, label='detrend')
-                # ax4.set_xticklabels(df_4H_mean['date'], rotation=30)
-                ax4.set_xticks(df_data_decom.index[::168])
-                ax4.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax4.legend()
-
-                ax7.plot(data_decom.seasonal, label='seasonal')
-                # ax7.set_xticklabels(df_4H_mean['date'], rotation=30)
-                ax7.set_xticks(df_data_decom.index[::168])
-                ax7.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax7.legend()
-
-                ax10.plot(data_decom.resid.abs(), label='resid')
-                # ax10.set_xticklabels(df_4H_mean['date'], rotation=30)
-                ax10.set_xticks(df_data_decom.index[::168])
-                ax10.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax10.legend()
+                ax0_2 = plt.subplot(gs[0:2, 2], sharex=ax0_0)
+                ax2_2 = plt.subplot(gs[2:4, 2])
 
 
+                ax0_3 = plt.subplot(gs[0, 3], sharex=ax0_0)
+                ax1_3 = plt.subplot(gs[1, 3], sharex=ax0_0)
+                ax2_3 = plt.subplot(gs[2, 3], sharex=ax0_0)
+                ax3_3 = plt.subplot(gs[3, 3], sharex=ax0_0)
 
-                # plt.plot(df_data_decom['observed'])
-                # plt.plot(df_data_decom['weekday'] * 100)
-                # plt.plot(df_data_decom['data_week_odd'], 'b')
-                # plt.plot(df_data_decom['data_week_even'], 'r')
-                # plt.plot(df_data_decom['max'], label='max')
-                # plt.plot(df_data_decom['min'], label='min')
-                # plt.plot(df_data_decom['mean'], label='mean')
-                # plt.plot(df_data_decom['std'], label='std')
-                # plt.legend()
-                # plt.show()
-                # plt.close()
+                ax0_4 = plt.subplot(gs[0, 4], sharex=ax0_0)
+                ax1_4 = plt.subplot(gs[1, 4], sharex=ax0_0)
+                ax2_4 = plt.subplot(gs[2, 4], sharex=ax0_0)
+                ax3_4 = plt.subplot(gs[3, 4], sharex=ax0_0)
 
 
-                ax2.plot(df_data_decom['resid_change_detection'], label='resid_change_detection')
-                # ax2.set_xticklabels(df_4H_mean['date'], rotation=30)
-                ax2.set_xticks(df_data_decom.index[::168])
-                ax2.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax2.legend()
+                xticklabels = df_data_decom.index[::168].strftime('%Y-%m-%d %H:%M:%s')
+                xlim_f = df_4H_mean.index[0] + datetime.timedelta(days=-7)
+                xlim_b = df_4H_mean.index[-1] + datetime.timedelta(days=7)
 
 
-                # ax5.plot(df_data_decom['reg_resid'], label='reg_resid')
-                # ax5.set_ylim(0, 1)
-                # # ax5.set_xticklabels(df_4H_mean['date'], rotation=30)
-                # ax5.legend()
+                ax0_0.plot(df_4H_mean.index, df_4H_mean['load'], label='observed')
+                ax0_0.plot(data_decom.trend, label='trend')
+                ax0_0.set_xticks(df_data_decom.index[::168])
+                ax0_0.set_xticklabels(xticklabels, rotation=30)
+                ax1_0.set_xlim(xlim_f, xlim_b)
+
+                ax1_0.plot(df_4H_mean.index, df_4H_mean['load'] - data_decom.trend, label='detrend')
+
+                ax2_0.plot(data_decom.seasonal, label='seasonal')
+
+                ax3_0.plot(data_decom.resid, label='resid')
 
 
-                # ax8.plot(df_data_decom['weekday'] * 100, label='weekday')
-                ax8.plot(df_data_decom['data_week_odd'], 'b', label='data_week_odd')
-                ax8.plot(df_data_decom['data_week_even'], 'r', label='data_week_even')
-                ax8.plot(df_data_decom['max'], label='max')
-                ax8.plot(df_data_decom['min'], label='min')
-                ax8.plot(df_data_decom['mean'], label='mean')
-                ax8.plot(df_data_decom['std'], label='std')
-                ax8.set_xticks(df_data_decom.index[::168])
-                ax8.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax8.legend()
+                ax0_1.hist(data_decom.resid.dropna())
+
+
+                # ax2_1.plot(df_data_decom['weekday'] * 100, label='weekday')
+                ax2_1.plot(df_data_decom['data_week_odd'], 'b', label='data_week_odd')
+                ax2_1.plot(df_data_decom['data_week_even'], 'r', label='data_week_even')
+                ax2_1.plot(df_data_decom['week_max'], label='week_max')
+                ax2_1.plot(df_data_decom['week_mean'], label='week_mean')
+                ax2_1.plot(df_data_decom['week_min'], label='week_min')
 
 
 
-                ax3.plot(df_4H_mean['diff_load'])
-                ax3.set_ylabel('4H Difference Load Data')
-                # ax3.set_xticklabels(df_4H_mean['date'], rotation=30)
-                ax3.set_xticks(df_data_decom.index[::168])
-                ax3.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax3.legend()
-
-                ax6.plot(df_4H_mean['diff_reg_load'])
-                ax6.set_ylabel('4H Difference Regular Load Data')
-                ax6.set_ylim(0, 1)
-                # ax6.set_xticklabels(df_4H_mean['date'], rotation=30)
-                ax6.set_xticks(df_data_decom.index[::168])
-                ax6.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax6.legend()
-
-                ax9.plot(df_4H_mean['mean_change_detection'])
-                ax9.set_ylabel('4H Mean Change Detection')
-                # ax9.set_xticklabels(df_4H_mean['date'], rotation=30)
-                ax9.set_xticks(df_data_decom.index[::168])
-                ax9.set_xticklabels(df_data_decom.index[::168], rotation=30)
-                ax9.legend()
+                ax0_2.plot(df_data_decom['day_max'], label='day_max')
+                ax0_2.plot(df_data_decom['day_mean'], label='day_mean')
+                ax0_2.plot(df_data_decom['day_min'], label='day_min')
 
 
+                ax2_2.hist(df_data_decom['day_max'].dropna())
+
+
+                ax0_3.plot(df_data_decom['diff_day_max'], label='diff_day_max')
+                ax1_3.plot(df_data_decom['diff_day_mean'], label='diff_day_mean')
+                ax2_3.plot(df_data_decom['diff_day_min'], label='diff_day_min')
+
+
+                ax0_4.plot(df_data_decom['diff_week_max'], label='diff_week_max')
+                ax1_4.plot(df_data_decom['diff_week_mean'], label='diff_week_mean')
+                ax2_4.plot(df_data_decom['diff_week_min'], label='diff_week_min')
+
+
+                # ax0_4.plot(df_4H_mean['diff_load'])
+                #
+                # ax1_4.plot(df_4H_mean['diff_reg_load'])
+                # ax1_4.set_ylim(0, 1)
+                #
+                # ax2_4.plot(df_4H_mean['mean_change_detection'])
+
+
+                for ax in fig.axes:
+                    plt.sca(ax)
+                    plt.xticks(rotation=30)
+                    plt.legend()
 
                 plt.legend()
-                # plt.savefig(dir_output + filepath.replace('C:\\_data\\부하데이터\\', '').replace('\\', '_').replace('.xls', '.png'))
+                plt.savefig(dir_output + filepath.replace('C:\\_data\\부하데이터\\', '').replace('\\', '_').replace('.xls', '.png'))
                 plt.show()
                 plt.close()
 
