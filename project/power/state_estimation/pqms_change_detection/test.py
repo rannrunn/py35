f995f97fb23c2ca72cdfe4ec20be3156e60230b2
Win10 <rannrunn@hanmail.net>
2018-08-13 05:06:02 +0900

    Modify : STL Decomposition, Change Detection

--- project/power/state_estimation/pqms_change_detection/test.py
+++ project/power/state_estimation/pqms_change_detection/test.py
@@ -1,20 +1,6 @@
-import numpy as np
-import pandas as pd
-from pandas.core.nanops import nanmean as pd_nanmean
-from statsmodels.tsa.seasonal import DecomposeResult
-from statsmodels.tsa.filters._utils import _maybe_get_pandas_wrapper_freq
-import statsmodels.api as sm
-
-observed = np.array([i for i in range(10, 20, 1)])
-
-detrended = np.array([i for i in range(10)])
-
-period = 2
 
 
-period_averages = np.array([pd_nanmean(detrended[i::period]) for i in range(period)])
-# 0-center the period avgs
-# 평균을 배열로 만들고 다시 평균을 계산
-period_averages -= np.mean(period_averages)
-#
-seasonal = np.tile(period_averages, len(observed) // period + 1)[:len(observed)]
+
+
+df_H_min = []
+
