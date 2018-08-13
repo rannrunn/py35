db2a4b9773a82e9c552ba0bb21f11716fb8a972a
rannrunn <rannrunn@hanmail.net>
2018-08-13 18:13:22 +0900

    Modify : PQMS Change Detection

--- project/power/state_estimation/pqms_change_detection/pqms_change_detection_one.py
+++ project/power/state_estimation/pqms_change_detection/pqms_change_detection_one.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import os
 import datetime
+import time
 
 
 import statsmodels.api as sm
@@ -66,6 +67,7 @@ def plot_pqms_decomposition(period):
 
 
     for filepath in filepaths:
+        start = time.time()
 
         dirname = os.path.dirname(filepath)
         filename = os.path.basename(filepath)
@@ -75,192 +77,173 @@ def plot_pqms_decomposition(period):
         df_temp = df_temp.rename(columns={'Unnamed: 1': 'load'})
 
 
+        print('경과시간 1:', (time.time() - start))
+
+
         bool_possible = is_possible(df_temp, 'load')
         if bool_possible and filename.find('3상') > -1: #
             df_temp.set_index(pd.to_datetime(df_temp[df_temp.columns[0]]), inplace=True)
-            df_H_max = df_temp.resample('H').max()
-            df_H_mean = df_temp.resample('H').mean()
-            df_H_min = df_temp.resample('H').min()
+            df_H_max = df_temp.resample('4H').max()
+            df_H_mean = df_temp.resample('4H').mean()
+            df_H_min = df_temp.resample('4H').min()
+            df_H_data = pd.DataFrame()
 
+
+            df_H_data['load_max'] = df_H_max['load']
+            df_H_data['load_mean'] = df_H_mean['load']
+            df_H_data['load_min'] = df_H_min['load']
 
 
             # 주별 통계량 max
-            df_H_max['datetime'] = df_H_max.index.values
-            df_H_max['weekday'] = df_H_max['datetime'].dt.weekday
-            df_H_max['day_of_year'] = df_H_max['datetime'].dt.dayofyear
-            df_H_max['day_of_years'] = df_H_max['datetime'].apply(lambda x: df_H_max.loc[x, 'day_of_year'] + sum([datetime.date(item, 12, 31).timetuple().tm_yday for item in list(set(df_H_max['datetime'].dt.year.values)) if item < df_H_max.loc[x, 'datetime'].year]))
+            df_H_data['datetime'] = df_H_data.index.values
+            df_H_data['weekday'] = df_H_data['datetime'].dt.weekday
+            df_H_data['day_of_year'] = df_H_data['datetime'].dt.dayofyear
+            df_H_data['day_of_years'] = df_H_data['datetime'].apply(lambda x: df_H_data.loc[x, 'day_of_year'] + sum([datetime.date(item, 12, 31).timetuple().tm_yday for item in list(set(df_H_data['datetime'].dt.year.values)) if item < df_H_data.loc[x, 'datetime'].year]))
 
 
-            df_H_max['date_group_week'] = df_H_max['datetime'].apply(lambda x: df_H_max.loc[x, 'day_of_years'] - (df_H_max.loc[x, 'weekday'] + 1))
-            df_H_max = df_H_max[(df_H_max['date_group_week'] != df_H_max['date_group_week'].min()) & (df_H_max['date_group_week'] != df_H_max['date_group_week'].max())]
-
-            df_H_max['data_week_odd'] = df_H_max.loc[df_H_max['date_group_week'] % 2 == 0, 'load']
-            df_H_max['data_week_even'] = df_H_max.loc[df_H_max['date_group_week'] % 2 == 1, 'load']
+            df_H_data['date_group_week'] = df_H_data['datetime'].apply(lambda x: df_H_data.loc[x, 'day_of_years'] - (df_H_data.loc[x, 'weekday'] + 1))
+            df_H_data = df_H_data[(df_H_data['date_group_week'] != df_H_data['date_group_week'].min()) & (df_H_data['date_group_week'] != df_H_data['date_group_week'].max())]
 
 
-            df_H_mean_des_week = pd.DataFrame(df_H_max.groupby('date_group_week')['load'].describe())
-            df_H_mean_des_week['date_group_week'] = df_H_mean_des_week.index
-            df_H_mean_des_week.rename(columns={'count': 'week_count', 'mean': 'week_mean', 'std': 'week_std', 'min': 'week_min', '25%': 'week_25%', '50%': 'week_50%', '75%': 'week_75%', 'max': 'week_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
-            df_H_max = pd.merge(df_H_max, df_H_mean_des_week, on='date_group_week', how='left')
+            df_H_data['data_week_odd'] = df_H_data.loc[df_H_data['date_group_week'] % 2 == 0, 'load_mean']
+            df_H_data['data_week_even'] = df_H_data.loc[df_H_data['date_group_week'] % 2 == 1, 'load_mean']
 
 
-            df_H_max['diff_week_max'] = df_H_max['week_max'].diff()
-            df_H_max['diff_week_mean'] = df_H_max['week_mean'].diff()
-            df_H_max['diff_week_min'] = df_H_max['week_min'].diff()
+            df_H_max_des_week = pd.DataFrame(df_H_data.groupby('date_group_week')['load_max'].describe())
+            df_H_max_des_week['date_group_week'] = df_H_max_des_week.index
+            # df_H_max_des_week.rename(columns={'count': 'week_count', 'mean': 'week_mean', 'std': 'week_std', 'min': 'week_min', '25%': 'week_25%', '50%': 'week_50%', '75%': 'week_75%', 'max': 'week_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
+            df_H_max_des_week.rename(columns={'max': 'week_max'}, inplace=True)
+            df_H_data_des_week = pd.DataFrame(df_H_max_des_week)
 
+
+            print('경과시간 2:', (time.time() - start))
 
 
             # 주별 통계량 mean
-            df_H_mean['datetime'] = df_H_mean.index.values
-            df_H_mean['weekday'] = df_H_mean['datetime'].dt.weekday
-            df_H_mean['day_of_year'] = df_H_mean['datetime'].dt.dayofyear
-            df_H_mean['day_of_years'] = df_H_mean['datetime'].apply(lambda x: df_H_mean.loc[x, 'day_of_year'] + sum([datetime.date(item, 12, 31).timetuple().tm_yday for item in list(set(df_H_mean['datetime'].dt.year.values)) if item < df_H_mean.loc[x, 'datetime'].year]))
-
-
-            df_H_mean['date_group_week'] = df_H_mean['datetime'].apply(lambda x: df_H_mean.loc[x, 'day_of_years'] - (df_H_mean.loc[x, 'weekday'] + 1))
-            df_H_mean = df_H_mean[(df_H_mean['date_group_week'] != df_H_mean['date_group_week'].min()) & (df_H_mean['date_group_week'] != df_H_mean['date_group_week'].max())]
-
-            df_H_mean['data_week_odd'] = df_H_mean.loc[df_H_mean['date_group_week'] % 2 == 0, 'load']
-            df_H_mean['data_week_even'] = df_H_mean.loc[df_H_mean['date_group_week'] % 2 == 1, 'load']
-
-
-            df_H_mean_des_week = pd.DataFrame(df_H_mean.groupby('date_group_week')['load'].describe())
+            df_H_mean_des_week = pd.DataFrame(df_H_data.groupby('date_group_week')['load_mean'].describe())
             df_H_mean_des_week['date_group_week'] = df_H_mean_des_week.index
-            df_H_mean_des_week.rename(columns={'count': 'week_count', 'mean': 'week_mean', 'std': 'week_std', 'min': 'week_min', '25%': 'week_25%', '50%': 'week_50%', '75%': 'week_75%', 'max': 'week_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
-            df_H_mean = pd.merge(df_H_mean, df_H_mean_des_week, on='date_group_week', how='left')
+            df_H_mean_des_week.rename(columns={'mean': 'week_mean'}, inplace=True)
+            df_H_data_des_week['week_mean'] = df_H_mean_des_week['week_mean']
 
 
-            df_H_mean['diff_week_max'] = df_H_mean['week_max'].diff()
-            df_H_mean['diff_week_mean'] = df_H_mean['week_mean'].diff()
-            df_H_mean['diff_week_min'] = df_H_mean['week_min'].diff()
 
+            print('경과시간 3:', (time.time() - start))
 
 
             # 주별 통계량 min
-            df_H_min['datetime'] = df_H_min.index.values
-            df_H_min['weekday'] = df_H_min['datetime'].dt.weekday
-            df_H_min['day_of_year'] = df_H_min['datetime'].dt.dayofyear
-            df_H_min['day_of_years'] = df_H_min['datetime'].apply(lambda x: df_H_min.loc[x, 'day_of_year'] + sum([datetime.date(item, 12, 31).timetuple().tm_yday for item in list(set(df_H_min['datetime'].dt.year.values)) if item < df_H_min.loc[x, 'datetime'].year]))
+            df_H_min_des_week = pd.DataFrame(df_H_data.groupby('date_group_week')['load_min'].describe())
+            df_H_min_des_week['date_group_week'] = df_H_min_des_week.index
+            df_H_min_des_week.rename(columns={'min': 'week_min'}, inplace=True)
+            df_H_data_des_week['week_min'] = df_H_min_des_week['week_min']
+            df_H_data_des_week['week_range'] = df_H_data_des_week['week_max'] - df_H_data_des_week['week_min']
 
 
-            df_H_min['date_group_week'] = df_H_min['datetime'].apply(lambda x: df_H_min.loc[x, 'day_of_years'] - (df_H_min.loc[x, 'weekday'] + 1))
-            df_H_min = df_H_min[(df_H_min['date_group_week'] != df_H_min['date_group_week'].min()) & (df_H_min['date_group_week'] != df_H_min['date_group_week'].max())]
-
-            df_H_min['data_week_odd'] = df_H_min.loc[df_H_min['date_group_week'] % 2 == 0, 'load']
-            df_H_min['data_week_even'] = df_H_min.loc[df_H_min['date_group_week'] % 2 == 1, 'load']
+            df_H_data = pd.merge(df_H_data, df_H_data_des_week, on='date_group_week', how='left')
+            df_H_data = pd.merge(df_H_data, df_H_data_des_week, on='date_group_week', how='left')
+            df_H_data = pd.merge(df_H_data, df_H_data_des_week, on='date_group_week', how='left')
 
 
-            df_H_mean_des_week = pd.DataFrame(df_H_min.groupby('date_group_week')['load'].describe())
-            df_H_mean_des_week['date_group_week'] = df_H_mean_des_week.index
-            df_H_mean_des_week.rename(columns={'count': 'week_count', 'mean': 'week_mean', 'std': 'week_std', 'min': 'week_min', '25%': 'week_25%', '50%': 'week_50%', '75%': 'week_75%', 'max': 'week_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
-            df_H_min = pd.merge(df_H_min, df_H_mean_des_week, on='date_group_week', how='left')
+            print('경과시간 4:', (time.time() - start))
 
 
-            df_H_min['diff_week_max'] = df_H_min['week_max'].diff()
-            df_H_min['diff_week_mean'] = df_H_min['week_mean'].diff()
-            df_H_min['diff_week_min'] = df_H_min['week_min'].diff()
+            df_H_data['diff_week_max'] = df_H_data['week_max'].diff()
+            df_H_data['diff_week_mean'] = df_H_data['week_mean'].diff()
+            df_H_data['diff_week_min'] = df_H_data['week_min'].diff()
 
 
 
-            # 일별 통계량
-            df_H_mean['yyyymmdd'] = df_H_mean['datetime'].dt.strftime('%Y%m%d')
-            df_data_decom_des_day = pd.DataFrame(df_H_mean.groupby('yyyymmdd')['load'].describe())
-            df_data_decom_des_day['yyyymmdd'] = df_data_decom_des_day.index
-            df_data_decom_des_day.rename(columns={'count': 'day_count', 'mean': 'day_mean', 'std': 'day_std', 'min': 'day_min', '25%': 'day_25%', '50%': 'day_50%', '75%': 'day_75%', 'max': 'day_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
-            df_H_mean = pd.merge(df_H_mean, df_data_decom_des_day, on='yyyymmdd', how='left')
-
-            df_H_mean['diff_day_max'] = df_H_mean['day_max'].diff()
-            df_H_mean['diff_day_mean'] = df_H_mean['day_mean'].diff()
-            df_H_mean['diff_day_min'] = df_H_mean['day_min'].diff()
+            # 일별 통계량 max
+            df_H_data['yyyymmdd'] = df_H_data['datetime'].dt.strftime('%Y%m%d')
+            df_H_max_des_day = pd.DataFrame(df_H_data.groupby('yyyymmdd')['load_max'].describe())
+            df_H_max_des_day['yyyymmdd'] = df_H_max_des_day.index
+            # df_H_max_des_day.rename(columns={'count': 'day_count', 'mean': 'day_mean', 'std': 'day_std', 'min': 'day_min', '25%': 'day_25%', '50%': 'day_50%', '75%': 'day_75%', 'max': 'day_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
+            df_H_max_des_day.rename(columns={'max': 'day_max'}, inplace=True)
+            df_H_data = pd.merge(df_H_data, df_H_max_des_day, on='yyyymmdd', how='left')
 
 
-            df_H_max.set_index(df_H_max['datetime'], inplace=True)
-            df_H_mean.set_index(df_H_mean['datetime'], inplace=True)
-            df_H_min.set_index(df_H_min['datetime'], inplace=True)
+            # 일별 통계량 mean
+            df_H_data['yyyymmdd'] = df_H_data['datetime'].dt.strftime('%Y%m%d')
+            df_H_mean_des_day = pd.DataFrame(df_H_data.groupby('yyyymmdd')['load_mean'].describe())
+            df_H_mean_des_day['yyyymmdd'] = df_H_mean_des_day.index
+            df_H_mean_des_day.rename(columns={'mean': 'day_mean'}, inplace=True)
+            df_H_data = pd.merge(df_H_data, df_H_mean_des_day, on='yyyymmdd', how='left')
 
 
-            df_H_max['date'] = df_H_max.index.to_series().dt.strftime('%Y-%m-%d').astype(str)
-            df_H_max.reindex([idx for idx in range(len(df_H_max))])
+            # 일별 통계량 min
+            df_H_data['yyyymmdd'] = df_H_data['datetime'].dt.strftime('%Y%m%d')
+            df_H_min_des_day = pd.DataFrame(df_H_data.groupby('yyyymmdd')['load_min'].describe())
+            df_H_min_des_day['yyyymmdd'] = df_H_min_des_day.index
+            df_H_min_des_day.rename(columns={'min': 'day_min'}, inplace=True)
+            df_H_data = pd.merge(df_H_data, df_H_min_des_day, on='yyyymmdd', how='left')
 
 
-            df_H_mean['date'] = df_H_mean.index.to_series().dt.strftime('%Y-%m-%d').astype(str)
-            df_H_mean.reindex([idx for idx in range(len(df_H_mean))])
+            df_H_data['day_range'] = df_H_data['day_max'] - df_H_data['day_min']
 
 
-            df_H_min['date'] = df_H_min.index.to_series().dt.strftime('%Y-%m-%d').astype(str)
-            df_H_min.reindex([idx for idx in range(len(df_H_min))])
+            df_H_data['diff_day_max'] = df_H_data['day_max'].diff()
+            df_H_data['diff_day_mean'] = df_H_data['day_mean'].diff()
+            df_H_data['diff_day_min'] = df_H_data['day_min'].diff()
+            df_H_data.set_index(df_H_data['datetime'], inplace=True)
+
+
+            df_H_data['date'] = df_H_data.index.to_series().dt.strftime('%Y-%m-%d').astype(str)
+            df_H_data.reindex([idx for idx in range(len(df_H_data))])
 
 
             # 일별 부하 데이터의 범위가 임계치를 초과하여 변동한 시점 탐지
-            df_range_day = df_H_mean.resample('D')['load'].max() - df_H_mean.resample('D')['load'].min()
+            df_range_day = df_H_data.resample('D')['load_max'].max() - df_H_data.resample('D')['load_min'].min()
 
 
-            fig = plt.figure(figsize=(35,14))
-            gs = gridspec.GridSpec(4, 5)
+            print('경과시간 5:', (time.time() - start))
+
+
+            fig = plt.figure(figsize=(21,14))
+            gs = gridspec.GridSpec(4, 3)
             ax0_0 = plt.subplot(gs[0, 0])
-            ax1_0 = plt.subplot(gs[1, 0], sharex=ax0_0)
+            ax1_0 = plt.subplot(gs[1, 0])
             ax2_0 = plt.subplot(gs[2, 0], sharex=ax0_0)
             ax3_0 = plt.subplot(gs[3, 0], sharex=ax0_0)
 
 
-            ax0_1 = plt.subplot(gs[0, 1])
+            ax0_1 = plt.subplot(gs[0, 1], sharex=ax0_0)
             ax1_1 = plt.subplot(gs[1, 1], sharex=ax0_0)
-
-
             ax2_1 = plt.subplot(gs[2, 1], sharex=ax0_0)
-            ax3_1 = plt.subplot(gs[3, 1])
+            ax3_1 = plt.subplot(gs[3, 1], sharex=ax0_0)
 
 
-            ax0_3 = plt.subplot(gs[0, 3], sharex=ax0_0)
-            ax1_3 = plt.subplot(gs[1, 3], sharex=ax0_0)
-            ax2_3 = plt.subplot(gs[2, 3], sharex=ax0_0)
-            ax3_3 = plt.subplot(gs[3, 3], sharex=ax0_0)
-
-            ax0_4 = plt.subplot(gs[0, 4], sharex=ax0_0)
-            ax1_4 = plt.subplot(gs[1, 4], sharex=ax0_0)
-            ax2_4 = plt.subplot(gs[2, 4], sharex=ax0_0)
-            ax3_4 = plt.subplot(gs[3, 4], sharex=ax0_0)
-
-
-            xticklabels = df_H_mean.index[::168].strftime('%Y-%m-%d %H:%M:%s')
-            xlim_f = df_H_mean.index[0] + datetime.timedelta(days=-7)
-            xlim_b = df_H_mean.index[-1] + datetime.timedelta(days=7)
-
+            ax0_2 = plt.subplot(gs[0, 2], sharex=ax0_0)
+            ax1_2 = plt.subplot(gs[1, 2], sharex=ax0_0)
+            ax2_2 = plt.subplot(gs[2, 2], sharex=ax0_0)
+            ax3_2 = plt.subplot(gs[3, 2], sharex=ax0_0)
 
 
             # ax2_1.plot(df_data_decom['weekday'] * 100, label='weekday')
-            ax1_1.plot(df_H_mean['data_week_odd'], 'b')
-            ax1_1.plot(df_H_mean['data_week_even'], 'r')
-            ax1_1.plot(df_H_max['week_max'])
-            ax1_1.plot(df_H_mean['week_mean'])
-            ax1_1.plot(df_H_min['week_min'])
+            ax0_0.plot(df_H_data['data_week_odd'], 'b')
+            ax0_0.plot(df_H_data['data_week_even'], 'r')
+            ax0_0.plot(df_H_data['week_max'])
+            ax0_0.plot(df_H_data['week_mean'])
+            ax0_0.plot(df_H_data['week_min'])
 
 
-
-            ax2_1.plot(df_H_mean['day_max'], label='day_max')
-            ax2_1.plot(df_H_mean['day_mean'], label='day_mean')
-            ax2_1.plot(df_H_mean['day_min'], label='day_min')
+            ax1_0.scatter(df_H_data_des_week.index, df_H_data_des_week['week_range'])
 
 
-            ax3_1.hist(df_H_mean['day_max'].dropna())
+            ax2_0.plot(df_H_data['day_max'], label='day_max')
+            ax2_0.plot(df_H_data['day_mean'], label='day_mean')
+            ax2_0.plot(df_H_data['day_min'], label='day_min')
 
 
-            ax0_3.plot(df_H_mean['diff_day_max'], label='diff_day_max')
-            ax1_3.plot(df_H_mean['diff_day_mean'], label='diff_day_mean')
-            ax2_3.plot(df_H_mean['diff_day_min'], label='diff_day_min')
+            ax3_0.scatter(df_H_data.index, df_H_data['day_range'])
 
 
-            ax0_4.plot(df_H_mean['diff_week_max'], label='diff_week_max')
-            ax1_4.plot(df_H_mean['diff_week_mean'], label='diff_week_mean')
-            ax2_4.plot(df_H_mean['diff_week_min'], label='diff_week_min')
+            ax0_1.scatter(df_H_data.index, df_H_data['diff_day_max'], s=10, label='diff_day_max')
+            ax1_1.scatter(df_H_data.index, df_H_data['diff_day_mean'], s=10, label='diff_day_mean')
+            ax2_1.scatter(df_H_data.index, df_H_data['diff_day_min'], s=10, label='diff_day_min')
 
 
-            # ax0_4.plot(df_4H_mean['diff_load'])
-            #
-            # ax1_4.plot(df_4H_mean['diff_reg_load'])
-            # ax1_4.set_ylim(0, 1)
-            #
-            # ax2_4.plot(df_4H_mean['mean_change_detection'])
+            ax0_2.scatter(df_H_data.index, df_H_data['diff_week_max'], s=10, label='diff_week_max')
+            ax1_2.scatter(df_H_data.index, df_H_data['diff_week_mean'], s=10, label='diff_week_mean')
+            ax2_2.scatter(df_H_data.index, df_H_data['diff_week_min'], s=10, label='diff_week_min')
 
 
             for ax in fig.axes:
@@ -268,14 +251,16 @@ def plot_pqms_decomposition(period):
                 plt.xticks(rotation=30)
                 plt.legend()
 
-            plt.legend()
             plt.savefig(dir_output + filepath.replace('C:\\_data\\부하데이터\\', '').replace('\\', '_').replace('.xls', '.png'))
             plt.show()
             plt.close()
 
 
+        print('경과시간 6:', (time.time() - start))
+
+
 if __name__ == '__main__':
-    period = 168
+    period = 42
     plot_pqms_decomposition(period)
 
 
--- project/power/state_estimation/pqms_change_detection/test.py
+++ project/power/state_estimation/pqms_change_detection/test.py
@@ -1,6 +1,21 @@
+df_H_data = []
+
+df_H_data['datetime'] = df_H_data.index.values
+df_H_data['weekday'] = df_H_data['datetime'].dt.weekday
+df_H_data['day_of_year'] = df_H_data['datetime'].dt.dayofyear
+df_H_data['day_of_years'] = df_H_data['datetime'].apply(lambda x: df_H_data.loc[x, 'day_of_year'] + sum([datetime.date(item, 12, 31).timetuple().tm_yday for item in list(set(df_H_data['datetime'].dt.year.values)) if item < df_H_data.loc[x, 'datetime'].year]))
 
 
+df_H_data['date_group_week'] = df_H_data['datetime'].apply(lambda x: df_H_data.loc[x, 'day_of_years'] - (df_H_data.loc[x, 'weekday'] + 1))
+df_H_data = df_H_data[(df_H_data['date_group_week'] != df_H_data['date_group_week'].min()) & (df_H_data['date_group_week'] != df_H_data['date_group_week'].max())]
+
+df_H_data['data_week_odd'] = df_H_data.loc[df_H_data['date_group_week'] % 2 == 0, 'load']
+df_H_data['data_week_even'] = df_H_data.loc[df_H_data['date_group_week'] % 2 == 1, 'load']
 
 
-df_H_min = []
+df_H_mean_des_week = pd.DataFrame(df_H_data.groupby('date_group_week')['load'].describe())
+df_H_mean_des_week['date_group_week'] = df_H_mean_des_week.index
+df_H_mean_des_week.rename(columns={'count': 'week_count', 'mean': 'week_mean', 'std': 'week_std', 'min': 'week_min', '25%': 'week_25%', '50%': 'week_50%', '75%': 'week_75%', 'max': 'week_max', 'yyyymmdd': 'yyyymmdd'}, inplace=True)
+df_H_data = pd.merge(df_H_data, df_H_mean_des_week, on='date_group_week', how='left')
 
+df_H_data = df_H_data
